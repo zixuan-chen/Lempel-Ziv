2374dd0420e7bc92353052595e117ff22ac83a71
Zixuan Chen <zchen4016@gmail.com>
2020-11-03 21:01:01 +0800

    update

---
+++ algorithms.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class LZ77:
+    def __init__(self, searchBuffSize, lookAheadBuffSize):
+        self.searchBuffSize = searchBuffSize
+        self.lookAheadBuffSize = lookAheadBuffSize
+
+        pass
+
+    def encode(self, input):
+        pass
+
+    def decode(self, ):
+        pass
+
